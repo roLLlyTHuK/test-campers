be34ff79aba55cf0a88dcac1710e2f7fccf08d31
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-04-21 07:04:50 +0000

    Deploying to gh-pages from @ roLLlyTHuK/test-campers@6d7f00664f8d60b6890c092b754e0289602720f9 🚀

---
+++ assets/FavoritesPage-9746ec24.js
@@ -0,0 +1,1 @@
+import{u as e,j as s,W as a}from"./index-afb73bff.js";import{s as t,O as o,C as i}from"./CampersGallery-84a371fd.js";import{P as n}from"./PagesStyles.styled-54e0ac3e.js";const c=()=>{const r=e(t);return s.jsx(a,{children:s.jsxs(n,{children:[s.jsx(o,{}),r.length>0?s.jsx(i,{campers:r}):s.jsx("h2",{children:"You haven't added any cars to your favorites"})]})})};export{c as default};
--- assets/PagesStyles.styled-54e0ac3e.js
+++ assets/PagesStyles.styled-54e0ac3e.js
@@ -1,4 +1,4 @@
-import{s as t}from"./index-e46775cf.js";const o=t.div`
+import{s as t}from"./index-afb73bff.js";const o=t.div`
   position: relative;
   display: flex;
   flex-direction: row;
