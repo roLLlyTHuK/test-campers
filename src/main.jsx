e2b0bd6c935aedfcd61f2258d9c44931e4c22a2e
roLLlyTHuK <ircvipua@gmail.com>
2024-04-21 09:17:37 +0300

    up

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter basename="/rent-campers">
+        <BrowserRouter basename="/test-campers/">
           <App />
         </BrowserRouter>
       </PersistGate>
