ae0013b4a32b3c570e444fe878645fb32ad52462
roLLlyTHuK <71319633+roLLlyTHuK@users.noreply.github.com>
2024-04-21 19:17:25 +0000

    Deploying to gh-pages from @ roLLlyTHuK/test-campers@f6982eb338aacb0dfda28815e808674360a8284b 🚀

--- assets/PagesStyles.styled-c12922c9.js
+++ assets/PagesStyles.styled-c12922c9.js
@@ -1,4 +1,4 @@
-import{s as t}from"./index-049eb9ad.js";const o=t.div`
+import{s as t}from"./index-8e5fe75d.js";const o=t.div`
   position: relative;
   display: flex;
   flex-direction: row;
@@ -12,23 +12,37 @@ import{s as t}from"./index-049eb9ad.js";const o=t.div`
   margin: 0 auto;
   max-width: 1440px;
 
-  h1 {
+  .text {
     position: absolute;
-    top: 50%;
-    left: 50%;
+    top: 40%;
+    left: 40%;
     transform: translate(-50%, -50%);
     z-index: 100;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    pointer-events: none;
+  }
+  .text > h1 {
     color: var(--color-yellow);
     font-size: 64px;
     font-weight: 600;
     line-height: 72px;
-    text-align: center;
-    pointer-events: none;
+    text-align: left;
+    margin-bottom: 24px;
+  }
+  .text > p {
+    color: var(--color-yellow);
+    font-size: 32px;
+    font-weight: 600;
+    line-height: 36px;
+    text-align: left;
   }
   .button {
     position: absolute;
-    top: 70%;
-    left: 50%;
+    top: 60%;
+    left: 20%;
     transform: translate(-50%, -50%);
     z-index: 100;
     color: var(--color-white);
@@ -57,7 +71,9 @@ import{s as t}from"./index-049eb9ad.js";const o=t.div`
     background-position: center center;
     opacity: 0.25;
     width: 30%;
-    transition: width 0.8s ease, opacity 0.5s ease;
+    transition:
+      width 0.8s ease,
+      opacity 0.5s ease;
     &:hover {
       width: 200%;
       opacity: 1;
